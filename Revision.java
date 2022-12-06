0c198c9df517cf2f75e2e5dc400bd8900db7b35e
Het Prajapati <hetprajapati80@gmail.com>
2022-12-06 22:15:19 +0530

    COMMIT

--- Revision.java
+++ Revision.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Revision {
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//        Revision f = new Revision();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(n+" X "+i+" = "+n*i);
+        }
+        int a = 20;
+        Integer i = Integer.valueOf(a);
+        System.out.println(i);
+        Integer j = a;
+        System.out.println(j);
+    }
 }
