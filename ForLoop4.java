8ddc56e820178e35a6f0a858cd6b4fff6bd3f8ae
Het Prajapati <hetprajapati80@gmail.com>
2022-12-06 22:22:58 +0530

    COMMIT

---
+++ ForLoop4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ForLoop4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int no = sc.nextInt();
+        for(int i=1;i<=no;i++) {
+            for(int space=i;space<=no;space++) {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i*2-1;j++) {
+                if(i==no||j==1||j==(i*2-1)){
+                    System.out.print("*");
+                }else {
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
