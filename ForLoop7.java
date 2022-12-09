58e12501b23e1901a32412645953ce64c3e7660b
Het Prajapati <hetprajapati80@gmail.com>
2022-12-09 13:36:21 +0530

    COMMIT

---
+++ ForLoop7.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ForLoop7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows: ");
+        int n = sc.nextInt();
+        for (int i=n;i>=1;i--)
+        {
+            for (int j=1;j<=i;j++)
+            {
+                if(j==1||i==n||j==i) {
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
