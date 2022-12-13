95357b6904c8668f3a0f95a9973b1a89d04a1b9f
Het Prajapati <hetprajapati80@gmail.com>
2022-12-13 07:56:15 +0530

    COMMIT

---
+++ StaticVariable.java
@@ -0,0 +1,6 @@
+public class StaticVariable {
+    public static void main(String[] args) {
+        VarType_Static obj = new VarType_Static();
+        obj.display();
+    }
+}
---
+++ VarType_Static.java
@@ -0,0 +1,10 @@
+public class VarType_Static {
+    public static final int id = 11;
+    public static final String name = "Het";
+    protected static int salary = 106550;
+    void display() {
+        System.out.println("id = "+id);
+        System.out.println("name = "+name);
+        System.out.println("salary = "+salary);
+    }
+}
